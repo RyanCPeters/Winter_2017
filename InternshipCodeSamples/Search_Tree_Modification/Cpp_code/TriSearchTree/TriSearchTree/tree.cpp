c89e93d569fe49304d3925168258eebcd0fb1e3c
Ryan Peters <petersRyan84@gmail.com>
2018-01-14 02:33:14 -0800

    apparently tree.cpp didn't get updated with the previous push to github

--- InternshipCodeSamples/Search_Tree_Modification/Cpp_code/TriSearchTree/TriSearchTree/tree.cpp
+++ InternshipCodeSamples/Search_Tree_Modification/Cpp_code/TriSearchTree/TriSearchTree/tree.cpp
@@ -29,24 +29,7 @@ Tree::Tree() {
 	root = nullptr;
 }
 
-Node* findNode(Node* seekPtr, Node* prevPtr, const int &val) {
-	while (seekPtr != nullptr && val != (seekPtr->val)) {
-		while (seekPtr != nullptr && val < seekPtr->val) {
-			prevPtr = seekPtr;
-			seekPtr = seekPtr->left;
-		}
-		while (seekPtr != nullptr && val > seekPtr->val) {
-			prevPtr = seekPtr;
-			seekPtr = seekPtr->right;
-		}
-	}
-	
-	if(seekPtr != nullptr) {
-		while (seekPtr->mid != nullptr) seekPtr = seekPtr->mid;
-		prevPtr = seekPtr;
-	}
-	return prevPtr;
-}
+
 
 void Tree::insert(int val) {
 	if (root == nullptr) {
@@ -54,7 +37,7 @@ void Tree::insert(int val) {
 	}
 	else {
 		Node *seekPtr = root;
-		while (seekPtr != nullptr && val != seekPtr->val) {
+		while (val != seekPtr->val) {
 			if (val < seekPtr->val) {
 				if (seekPtr->left == nullptr) {
 					seekPtr->left = new Node(val);
@@ -73,40 +56,102 @@ void Tree::insert(int val) {
 					delete[] seekPtr;
 					return;				
 				}
-			}
-		}
-		/*
-		if (val == root->val) {
-			if (root->mid == nullptr) {
-				root->mid = new Node(val);
-				prevPtr = seekPtr = nullptr;
-				delete[] prevPtr;
-				delete[] seekPtr;
-				return;
-			}
-			seekPtr = root->mid;
-			while(seekPtr->mid != nullptr) seekPtr = seekPtr->mid;
+				else {
+					seekPtr = seekPtr->right;
+				}
+			} // end of if(val < seekPtr->val)-else block
+		} // end while loop
+		if (val == seekPtr->val) {
+			while(seekPtr->mid != nullptr)seekPtr = seekPtr->mid;
 			seekPtr->mid = new Node(val);
-			if (val == seekPtr->val) seekPtr->mid = new Node(val);
-			else if(val < seekPtr->val)seekPtr->left = new Node(val);
-			else if(val > seekPtr->val)seekPtr->right = new Node(val);
-			prevPtr = seekPtr = nullptr;
-			delete[] prevPtr;
-			delete[] seekPtr;
 			return;
 		}
-		else if (val < root->val) seekPtr = root->left;		
-		else	seekPtr = root->right;
-		seekPtr = findNode(seekPtr,prevPtr,val);
-
-		if (seekPtr == nullptr) {
-			if (val < prevPtr->val) prevPtr->left = new Node(val);
-			else if (val > prevPtr->val) prevPtr->right = new Node(val);
-		}
-		*/
 	}
 }
 
 void Tree::remove(int val) {
+	if (root->val == val) {
+		delete[] root;
+		root = nullptr;
+	}
+	else {
+		Node *seekPtr = root, *prevPtr;
+		int direction = 0; // -1 for left, 0 for mid, 1 for right;
+		while (val != seekPtr->val) {
+			if (val < seekPtr->val) {
+				if (seekPtr->left == nullptr) {
+					return;
+				}
+				else {
+					direction = -1;
+					prevPtr = seekPtr;
+					seekPtr = seekPtr->left;
+				}
+			}
+			else {
+				if (seekPtr->right == nullptr) {
+					return;
+				}
+				else {
+					direction = 1;
+					prevPtr = seekPtr;
+					seekPtr = seekPtr->right;
+				}
+			} // end of if(val < seekPtr->val)-else block
+		} // end while loop
+		if (val == seekPtr->val) {
+			if(seekPtr->mid != nullptr){
+				direction = 0;
+				while (seekPtr->mid != nullptr)seekPtr = seekPtr->mid;	
+				delete[](seekPtr->mid);
+				seekPtr = prevPtr = nullptr;
+				delete[] seekPtr;
+				delete[] prevPtr;
+				return;
+			}
+			else if (seekPtr->left == nullptr && seekPtr->right == nullptr) {
+				prevPtr = nullptr;
+				delete[](prevPtr);
+				delete[] seekPtr;
+				return;
+			}
+			else  {
+				Node *seekPtr2, *prevPtr2;
+				if (seekPtr->left != nullptr) { 
+					seekPtr2 = seekPtr->left->right, prevPtr2 = seekPtr->left;
+					// we need to find the node containing the natural predecessor value to to the current node's value
+					if (seekPtr->left->right != nullptr) {
+						while (seekPtr2->right != nullptr) {
+							prevPtr2 = seekPtr2;
+							seekPtr2 = seekPtr2->right;
+						}
+						prevPtr2->right = seekPtr2->left;
+					}
+				}
+				else if (seekPtr->right != nullptr) {
+					if (seekPtr->right->left != nullptr) {
+						seekPtr2 = seekPtr->right->left, prevPtr2 = seekPtr->right;
+					
+						while (seekPtr2->left != nullptr) {
+							prevPtr2 = seekPtr2;
+							seekPtr2 = seekPtr2->left;
+						}
+						prevPtr2->left = seekPtr2->right;
+					}
+				}
+				seekPtr2->left = seekPtr->left;
+				seekPtr2->right = seekPtr->right;
+				prevPtr = seekPtr;
+				seekPtr2 = prevPtr2 = prevPtr = seekPtr->left = seekPtr->right = nullptr;
+				
+				delete[] prevPtr;
+				delete[] prevPtr2;
+				delete[] seekPtr;
+				delete[] seekPtr2;
+
+			}
+			return;
+		}
 	
+	}
 }
