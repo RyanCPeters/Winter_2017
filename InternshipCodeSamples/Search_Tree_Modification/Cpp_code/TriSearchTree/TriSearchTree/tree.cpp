14a44497dbf61522af1f3674329e46eb947b8043
Ryan Peters <petersRyan84@gmail.com>
2018-01-14 01:11:45 -0800

    added the provided sample input/output testing files, and triSearchTree is nearly done

--- InternshipCodeSamples/Search_Tree_Modification/Cpp_code/TriSearchTree/TriSearchTree/tree.cpp
+++ InternshipCodeSamples/Search_Tree_Modification/Cpp_code/TriSearchTree/TriSearchTree/tree.cpp
@@ -26,13 +26,87 @@ Node * Tree::getRoot() { return root; }
 * Implement the methods below.
 */
 Tree::Tree() {
-	root = new Node();
+	root = nullptr;
+}
+
+Node* findNode(Node* seekPtr, Node* prevPtr, const int &val) {
+	while (seekPtr != nullptr && val != (seekPtr->val)) {
+		while (seekPtr != nullptr && val < seekPtr->val) {
+			prevPtr = seekPtr;
+			seekPtr = seekPtr->left;
+		}
+		while (seekPtr != nullptr && val > seekPtr->val) {
+			prevPtr = seekPtr;
+			seekPtr = seekPtr->right;
+		}
+	}
+	
+	if(seekPtr != nullptr) {
+		while (seekPtr->mid != nullptr) seekPtr = seekPtr->mid;
+		prevPtr = seekPtr;
+	}
+	return prevPtr;
 }
 
 void Tree::insert(int val) {
-	if(root == nullptr)
+	if (root == nullptr) {
+		root = new Node(val);
+	}
+	else {
+		Node *seekPtr = root;
+		while (seekPtr != nullptr && val != seekPtr->val) {
+			if (val < seekPtr->val) {
+				if (seekPtr->left == nullptr) {
+					seekPtr->left = new Node(val);
+					seekPtr = nullptr;
+					delete[] seekPtr;
+					return;
+				}
+				else {
+					seekPtr = seekPtr->left;
+				}
+			}
+			else {
+				if (seekPtr->right == nullptr) {
+					seekPtr->right = new Node(val);
+					seekPtr = nullptr;
+					delete[] seekPtr;
+					return;				
+				}
+			}
+		}
+		/*
+		if (val == root->val) {
+			if (root->mid == nullptr) {
+				root->mid = new Node(val);
+				prevPtr = seekPtr = nullptr;
+				delete[] prevPtr;
+				delete[] seekPtr;
+				return;
+			}
+			seekPtr = root->mid;
+			while(seekPtr->mid != nullptr) seekPtr = seekPtr->mid;
+			seekPtr->mid = new Node(val);
+			if (val == seekPtr->val) seekPtr->mid = new Node(val);
+			else if(val < seekPtr->val)seekPtr->left = new Node(val);
+			else if(val > seekPtr->val)seekPtr->right = new Node(val);
+			prevPtr = seekPtr = nullptr;
+			delete[] prevPtr;
+			delete[] seekPtr;
+			return;
+		}
+		else if (val < root->val) seekPtr = root->left;		
+		else	seekPtr = root->right;
+		seekPtr = findNode(seekPtr,prevPtr,val);
+
+		if (seekPtr == nullptr) {
+			if (val < prevPtr->val) prevPtr->left = new Node(val);
+			else if (val > prevPtr->val) prevPtr->right = new Node(val);
+		}
+		*/
+	}
 }
 
 void Tree::remove(int val) {
-
+	
 }
