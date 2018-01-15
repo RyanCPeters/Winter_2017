592ba2512f7757d272205d9e3bbfac36558f2de3
Ryan Peters <petersryan84@gmail.com>
2018-01-15 09:33:55 -0800

    added a random number generator as a submodule

---
+++ css_343/ass1_Turtle_Program/cmake-build-debug/CMakeFiles/ass1_Turtle_Program.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ass1_Turtle_Program.dir/turtleprogram/turtleprogram/main.cpp.obj"
+  "CMakeFiles/ass1_Turtle_Program.dir/turtleprogram/turtleprogram/turtleprogram.cpp.obj"
+  "ass1_Turtle_Program.pdb"
+  "ass1_Turtle_Program.exe"
+  "ass1_Turtle_Program.exe.manifest"
+  "libass1_Turtle_Program.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ass1_Turtle_Program.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ practice_code/leetcode/Median_of_Two_Sorted_Arrays/Median/Median/median.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <vector>
+
+using namespace std;
+
+class Solution {
+	struct Positions;
+public:
+	double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+		Positions n1Pos, n2Pos;
+
+		
+	}
+
+private:
+	struct Poisitions { int leftLen = 0, rightLen = 0, totalLen = 0; };
+};
